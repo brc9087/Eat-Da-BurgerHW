0cd94ad4208af70a2d3f0b2547f4f2f59bda69a4
Brian Cardines <bcardines811@gmail.com>
2020-12-05 03:32:23 -0500

    need to fix devour button and json showing up as undefined

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('mcRib', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('california burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bufalo burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('shake shack schroom', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('five guys burger', false)
